d99103587118026e4fc973f843962072849c08b0
jeremihz <mihjere68@gmail.com>
2019-09-02 12:55:53 +0300

    site

--- public/js/app.js
+++ public/js/app.js
@@ -60489,8 +60489,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/kryme/Documents/newtrans/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/kryme/Documents/newtrans/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\wamp64\www\TransWeb\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\wamp64\www\TransWeb\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
